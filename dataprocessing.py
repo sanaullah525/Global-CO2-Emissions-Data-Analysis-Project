ebaf123811fb6756e2f2b0fd37eaebf3ab04e2f5
sanaullah525 <sanasanaullah524@gmail.com>
2026-03-15 23:57:18 +0500

    Data imported to the virtual environment.

---
+++ dataprocessing.py
@@ -0,0 +1,9 @@
+# Importing all the necessary libraries for data processing.
+
+import pandas as pd
+import numpy as np
+import openpyxl
+
+
+df= pd.read_excel(r"C:\Users\dell\Desktop\CO2-dataset.xlsx")
+print(df.head(50))
